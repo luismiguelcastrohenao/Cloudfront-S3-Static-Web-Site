ead67694e2455e326c1dfb48bd23f2c29f407d0f
luismiguelcastrohenao <luismiguelcastrohenao@gmail.com>
2025-09-11 05:55:17 -0500

    Feat: Update outputs.tf - Added cloudfront_distribution_id

--- .iac/outputs.tf
+++ .iac/outputs.tf
@@ -5,3 +5,7 @@ output "bucket_name" {
 output "cloudfront_domain" {
   value = aws_cloudfront_distribution.s3_distribution.domain_name
 }
+
+output "cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.s3_distribution.id
+}
